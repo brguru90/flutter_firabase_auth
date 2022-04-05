a249b135d3969b5b475a9cf186db2ed97a6c49d8
Guruprasad BR <brguru90@gmail.com>
2022-04-05 16:59:15 +0530

    .

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:social_auth/screens/login/screen.dart';
+import 'package:social_auth/screens/signUp/screen.dart';
+
+void main() async {
+  runApp(MaterialApp(
+    initialRoute: "/",
+    routes: {
+      "/": (context) => LoginScreen(),
+      "/sign_up": (context) => signUpScreen(),
+    },
+  ));
+}
